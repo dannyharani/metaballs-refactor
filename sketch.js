7705eb9bccbc17ae4ce5763e25aa7afe5746013e
Hadi1723 <74918776+Hadi1723@users.noreply.github.com>
2020-12-31 01:24:55 -0500

    Update sketch.js
    attempted to make this file more object-oriented

--- sketch.js
+++ sketch.js
@@ -1,17 +1,35 @@
+var blobs = []
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 200);
+  colorMode(HSB);
+  for (i = 0; i < 10; i++) blobs.push(new Blob(random(0, width), random(0, height)));
 }
 
 function draw() {
   //background(220);
 
-  loadPixels();
+  //loadPixels();
   
   for(var xPix = 0; xPix < width; xPix++) {
     for(var yPix = 0; yPix < height; yPix++) {
-      set(xPix, yPix, color(xPix % 255, yPix % 255, 255))
+      let sum = 0;
+      for (i = 0; i < blobs.length; i++) {
+        let xdif = xPix - blobs[i].position.x;
+        let ydif = yPix - blobs[i].position.y;
+        let d = sqrt((xdif * xdif) + (ydif * ydif));
+        sum += 10 * blobs[i].r / d;
+        
+      }
+      set(xPix, yPix, color(xPix % 255, yPix % 255, 255));
+      //set(xPix, yPix, color(sum, 255, 255));
     }
   }
 
   updatePixels();
+  
+  for (i = 0; i < blobs.length; i++) {
+    blobs[i].update();
+    blobs[i].checkEdges();
+  }
 }
