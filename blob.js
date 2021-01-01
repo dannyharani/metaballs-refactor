3c2ee87979c330883c9e9ddbf2b4b9d53a247d14
Danny Harani <danniharany@gmail.com>
2021-01-01 16:34:43 -0500

    Update blob.js
    Added force handling methods and removed show method that was not needed.

--- blob.js
+++ blob.js
@@ -1,30 +1,34 @@
-var Blob = function(x, y) {
-  this.position = new createVector(x, y);
-  let angle = random(0, 2 * PI);
-  this.velocity = new createVector(random(2, 5) * Math.cos(angle), random(2, 5) * Math.sin(angle));
-  this.acceleration = new createVector(5, 5);
-  this.r = random(120, 240);
-}
+var Blob = function(mass) {
 
-Blob.prototype.update = function() { 
-  this.velocity.add(this.acceleration);
-  this.velocity.limit(60);
-  this.position.add(this.velocity);
-  this.acceleration.mult(0);
-}
+    this.mass = mass;
 
-Blob.prototype.checkEdges = function() {
-  if (this.position.x > width || this.position.x < 0) {
-    this.velocity.x *= -1;
-    
-  } else if (this.position.y > height || this.position.y < 0) {
-    this.velocity.y *= -1;
-  }
+    this.position = createVector(random(0, width), random(0, height));
+    this.velocity = createVector(0, 0);
+    this.acceleration = createVector(0, 0);
+
+    this.r = map(mass, 1, 12, 400, 1000);
+
 };
 
-Blob.prototype.show = function() {
-  noFill();
-  stroke(0);
-  strokeWeight(4);
-  ellipse(this.position.x / map(noise(0, 1), 0, 1, 100, width/2), this.position.y / map(noise(0, 1), 0, 1, 100, height/2), this.r * 2, this.r * 2);
-}
+Blob.prototype.update = function() {
+
+    this.velocity.add(this.acceleration);
+
+    this.velocity.limit(8);
+
+    this.position.add(this.velocity);
+};
+
+Blob.prototype.addForce = function(pos, mass) {
+    var position = createVector(this.position.x, this.position.y);
+
+    var force = position.sub(pos);
+    
+    var dist = force.mag();
+    dist = constrain(dist, 1, 10);
+
+    force.normalize();
+    force.mult(G * this.mass * mass / (dist * dist));
+
+    this.acceleration.add(force.mult(-1));
+};
