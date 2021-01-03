d6764c795b986934a435847a73b0cb9045dc5cf9
Danny Harani <danniharany@gmail.com>
2021-01-02 23:13:06 -0500

    Add files via upload

---
+++ Sun.js
@@ -0,0 +1,19 @@
+var Sun = function() {
+    this.itterator = 0;
+    this.position = createVector(width/2, height/2);
+    this.mass = 10;
+};
+
+Sun.prototype.orbit = function() {
+    this.position.x = cos(this.itterator);
+    this.position.y = sin(this.itterator);
+
+    this.position.mult(150);
+    this.position.add(200, 200);
+    this.itterator += 0.01;
+};
+
+Sun.prototype.show = function() {
+    fill('blue');
+    ellipse(this.position.x, this.position.y, 40, 40);
+};
