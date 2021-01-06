378b94e0717566df2278eb47722c965d132baa58
Danny Harani <danniharany@gmail.com>
2021-01-06 10:35:20 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ var G = 1.7;
 var sun;
 
 setup = function() {
+  frameRate(24);
   createCanvas(400, 400);
   colorMode(HSB);
   for (var i = 0; i < 6; i++) {
@@ -25,17 +26,17 @@ draw = function() {
   
   loadPixels();
   
-  for (var x = 0; x < width; x+=4) {
-    for(var y = 0; y < height; y+=4) {
+  for (var x = 0; x < width; x+=2) {
+    for(var y = 0; y < height; y+=2) {
       let sum = 0;
 
       for(var i = 0; i < blobs.length; i++) {
         var d = blobs[i].position.dist(createVector(x, y));
         sum += 5 * blobs[i].r / d;
       }
-      
-      for(var i = 0; i < 4; i++) {
-        for(var j = 0; j < 4; j++) {
+
+      for(var i = 0; i < 2; i++) {
+        for(var j = 0; j < 2; j++) {
           set(x+i, y+j, color(sum, 255, 255));
         }
       }
@@ -46,16 +47,19 @@ draw = function() {
   updatePixels();
   
   for(var i = 0; i < blobs.length; i++) {
+    //for(var j = 0; j < blobs.length; j++) {
+
+      //blobs[i].addForce(blobs[j].position, blobs[j].mass);
+      
+      
+    //}
     blobs[i].addForce(sun.position, sun.mass);
     blobs[i].update();
     //blobs[i].checkEdges();
-    /*  for(var j = 0; j < blobs.length; j++) {
-      blobs[i].addForce(blobs[j].position, blobs[j].mass);
-      }
-     */
   }
   
   sun.orbit();
   sun.show();
 
 };
+
